9058344802ed62c8089641320edef2c4a502f017
03jia <116247679+03jia@users.noreply.github.com>
2023-05-19 11:43:20 +0800

    Update description General.vue

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -24,7 +24,7 @@ const avatar = ref(userInfo.value.avatar ?? '')
 
 const name = ref(userInfo.value.name ?? '')
 
-const description = ref(userInfo.value.description ?? '')
+const description = ref('https://github.com/03jia/chatgpt-web')
 
 const language = computed({
   get() {
